1ad0f0aca96f916343f53f12045adac45f2acdaf
Jens Kai <jkaih7@gmail.com>
2018-11-01 02:39:46 -0400

    first property bind

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,22 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]' - makes div attribute
   // selector: '.app-servers' - makes class attribute
   selector: 'app-servers', // placing [...] or .- - - around the selector makes it an atribute. in the html <div <app-servers></div>
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`, // template instead of templateURL (external file) if you have 3 lines or less of html code
+
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,  template instead of templateURL (external file) if you have 3 lines or less of html code
+  templateUrl: './servers.component.html',
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
